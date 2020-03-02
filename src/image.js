97d6f68c921daa620d944fd4166f7b950630fe61
Colby Fayock <fayock@gmail.com>
2020-03-01 21:00:49 -0500

    first commit

---
+++ src/image.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const { respondOk, respondNotFound, respondError, getImageBuffer } = require('../lib/request');
+const { contructCloudinaryUrl } = require('../lib/cloudinary');
+const images = require('./images');
+
+/**
+ * handler
+ * @description Lambda handler
+ */
+
+export async function handler({ path } = {}) {
+  const pathParts = path.split('/');
+  const pathImageId = pathParts[2];
+  const imageConfig = images.find(({ imageId } = {}) => imageId === pathImageId);
+
+  if ( !imageConfig ) {
+    return respondNotFound({
+      message: `Not Found: Can not find imageId ${pathImageId}`
+    });
+  }
+
+  let imageUrl;
+
+  try {
+    imageUrl = contructCloudinaryUrl({
+      ...imageConfig,
+      cloudName: process.env.CLOUDINARY_CLOUD_NAME
+    });
+  } catch(e) {
+    return respondError({
+      message: e.message
+    });
+  }
+
+  return respondOk({
+    statusCode: 302,
+    headers: {
+      Location: imageUrl
+    }
+  });
+}
